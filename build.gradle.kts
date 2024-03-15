828e739c9c3ee6d914bdf95fd72e5958ec0c5f47
Alejandro-git-2022 <106359817+Alejandro-git-2022@users.noreply.github.com>
2024-03-15 03:01:10 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,19 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.2" apply false
 }
+
+
+android {
+    // Otras configuraciones para tu proyecto Android
+    
+    // Configuración para ignorar el archivo google-services.json
+    applicationVariants.all { variant ->
+        variant.resolvedConfigurations.forEach { config ->
+            config.dependencies.forEach { dep ->
+                if (dep.name == 'google-services') {
+                    dep.exclude group: 'com.google.gms.google-services', module: 'google-services'
+                }
+            }
+        }
+    }
+}
